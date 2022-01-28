17b654dc9a97d6ef60aa6fad4d8672fbab2ff1e0
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-01-28 16:09:50 +0100

    chore: Added BrowserRouter and Routes to main before branching.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, world</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
