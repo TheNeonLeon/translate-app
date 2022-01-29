27dd42fb455e2755a0215c76db9379fc3a19b667
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-01-29 15:24:03 +0100

    CHORE: add requiring import in App.js for routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './views/LoginPage';
+import TranslationPage from './views/TranslationPage';
+import ProfilePage from './views/ProfilePage';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
+
+          <Route path = "/" element = {<LoginPage/>}></Route>
+          <Route path = "/translation" element = {<TranslationPage/>}></Route>
+          <Route path = "/profile" element = {<ProfilePage/>}></Route>
           
         </Routes>
       </div>
