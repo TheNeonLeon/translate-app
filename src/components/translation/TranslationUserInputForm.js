f8a9ab7f34c6922ae4d30a5750403183786816ca
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-01-29 17:46:06 +0100

    chore: Built up basic form for input text into translation form.

--- src/components/translation/TranslationUserInputForm.js
+++ src/components/translation/TranslationUserInputForm.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import {useForm} from 'react-hook-form'
 
-const TranslationUserInputForm = () => {
-  return <div></div>;
+const translationConfig = {
+    required: true
+}
+const TranslationUserInputForm = (props) => {
+    const {translate, handleSubmit, formState: {errors}} = useForm();
+
+    const onSubmit = ({translationInput}) => {
+        
+        props.sendTranslation(translationInput);
+    }
+
+    const errorMessage = (() => {
+        if(!errors.translationInput){
+            return null;
+        }
+        if(errors.translationInput.type === 'required'){
+            return <span>Input is required for a translation to be performed!</span>;
+        }
+    })()
+
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <fieldset>
+                    <label htmlFor="translationInput">Text to translate: </label>
+                    <input type="text"  {...translate("translationInput", translationConfig)} />
+                    {errorMessage}
+                </fieldset>
+                <button type="submit" >Translate</button>
+            </form>
+        </>
+    )
 };
 
 export default TranslationUserInputForm;
--- src/views/TranslationPage.js
+++ src/views/TranslationPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TranslationUserInputForm from '../components/translation/TranslationUserInputForm';
+import TranslationUserInputForm from '../components/Translation/TranslationUserInputForm';
 
 const TranslationPage = () => {
   return (
