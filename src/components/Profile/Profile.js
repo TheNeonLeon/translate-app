088f07de900df97e7e01034599d586fae0dc4b6c
Leon Listo <Leon.listo@hotmail.com>
2022-01-30 12:30:08 +0100

    CHORE: Added some basic elements for profile components

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+ const Profile = () => {
+  return (
+    <>
+        <h1>Profile</h1>
+        <button>Delete translations</button>
+    </>
+  )
+};
+
+export default Profile
--- src/views/ProfilePage.js
+++ src/views/ProfilePage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Profile from '../components/Profile/Profile'
 
 const ProfilePage = () => {
   return (
   <>
-  <h1>Profile</h1>
+    <Profile />
   </>
   );
 };
