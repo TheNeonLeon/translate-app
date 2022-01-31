c447d65ca3d8bb029794442abd37402e4a77d54b
Leon Listo <Leon.listo@hotmail.com>
2022-01-31 14:54:40 +0100

    Update TranslationList.js

--- src/components/Profile/TranslationList.js
+++ src/components/Profile/TranslationList.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { TranslationContext } from '../../context/TranslationsContext';
 
  const TranslationList = () => {
+
+  const {translations} = TranslationContext()
   return (
+    <TranslationList.Consumer>
   <div>
       <ul>
-          
+          {translations}
       </ul>
   </div>
+  </TranslationList.Consumer>
   );
 };
 
