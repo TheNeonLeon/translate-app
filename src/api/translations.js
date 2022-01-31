6b1cdea25a8cc2cb83c96085e542872b4205b322
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-01-31 14:48:42 +0100

    feat: When translations are performed the text and a deleted-marker are stored in localstorage.

---
+++ src/api/translations.js
@@ -0,0 +1,9 @@
+const apiURL = process.env.REACT_APP_BASE_API_URL;
+
+const patchTranslations = () => {
+    try {
+        const response = fetch(`${apiURL}`)
+    } catch (error) {
+        
+    }
+}
