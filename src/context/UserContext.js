04f9db0ec0131e16da6927d9664f398351f2683c
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-01-31 15:38:30 +0100

    CHORE: import from storage and storageKeys

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,4 +1,6 @@
 import { createContext, useContext, useState } from "react";
+import { readTheStorage } from "../storage/storage";
+import { STORAGE_KEY_USER } from "../storage/storageKeys";
 
 //UserContext - a context to exposing the value
 const UserContext = createContext();
@@ -11,7 +13,7 @@ export const useUser = () => {
 //UserProvider - a provider to managing state
 const UserProvider = ({ children }) => {
 
-    const [ user, setUser ] = useState(null);
+    const [ user, setUser ] = useState(readTheStorage(STORAGE_KEY_USER));
 
     const state = {
         user,
