499aa363fa7105b6c1244b9f60077ac08a72b8b8
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-01-31 14:53:50 +0100

    FEAT: add provider in UserContext

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,3 +1,18 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 const UserContext = createContext();
+
+//exposing the value
+export const useUser = () => {
+    return useContext(UserContext);
+}
+
+
+const UserProvider = () => {
+    return (
+        <UserContext.Provider>
+
+        </UserContext.Provider>
+    )
+}
+export default UserProvider;
