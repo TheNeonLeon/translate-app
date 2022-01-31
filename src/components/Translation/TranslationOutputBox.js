d1bdd67d811cdb12c3dc9e1670afff305172df39
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-01-31 08:50:50 +0100

    fix: Moved assets and made showing images work in TranslationOutputBox

--- src/components/Translation/TranslationOutputBox.js
+++ src/components/Translation/TranslationOutputBox.js
@@ -3,14 +3,13 @@ const TranslationOutputBox = ({translation}) => {
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     const translate = translation.map((character, index) => {
         if(!alphabet.includes(character)){
-            return <></>;
+            return;
         }
         const char = character.toLowerCase();
-        const filepath = "./"+char+".png"
-        console.log(filepath);
+        const filepath = "assets/"+char+".png" 
         return (
             //<span key={index}><img src={`.../assets/${char}.png`} alt={`A sign-language-sign corresponding to the letter ${char}`}></img></span>
-            <span key={index}><img contentEditable="false" src={filepath} width="120" height="100"/></span>
+            <span key={index}><img contentEditable="false" src={filepath} alt='A sign-language-sign corresponding to a letter of the alphabet.' width="70" height="70"/></span>
         );
     }); //{translation.length > 0 ? translate : <></>}
 
