03884c524e1338d1542055183038ff62c470b7ed
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-01-31 14:27:08 +0100

    CHORE: add imports, states, loading text and errormessage
    add imports for;  localStorage, useEffect to navigate to other pages, userLogin from user.js and Login.css.
    add states for showing loading text and API error message and implement the states in the onSubmit form

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,8 +1,10 @@
 //Imports
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { appendErrors, useForm } from "react-hook-form";
 import { Navigate } from "react-router-dom";
-import { userLogin } from '../../api/user'
+import { userLogin } from '../../api/user';
+import { readTheStorage, saveToStorage } from "../../storage/storage";
+import './StyleFolder/Login.css';
 
 //Variable that shows the user needs to input a name
 const usernameRequirement = {
@@ -11,47 +13,58 @@ const usernameRequirement = {
 
 //LoginForm - Function to Login
 const LoginForm = () => {
-
     const {
         register,
         handleSubmit,
         formState: { errors }
     } = useForm();
 
+    //state for showing you pressed the button and are continuing
+    const [ loadingText, setLoadingText ] = useState(false);
+    //state for showing API error message
+    const [ errorMessageApi, setApiError ] = useState(null);
+
+    //useEffect - side effect to navigate to Translation Page if input of name was successful
+    useEffect(() => {}, [])
+
     //onSubmit - function that prevents page from reloading
     const onSubmit = async ({ username }) => {
-        const [ error, user] = await userLogin(username)
-        console.log('Error in the login:', error)
-        console.log('The user', user)
+        setLoadingText(true)
+        const [ error, user ] = await userLogin(username)
+
+        if (error !== null) {
+            setApiError(error)
+        }
+        if (user !== null) {
+            saveToStorage('LostInTranslation-User', user)
+        }
+        setLoadingText(false)
     };
 
-    console.log(errors)
-
-    //displayErrorMessage - function that display error message if th username requirements is not succeeding
+    //displayErrorMessage - render function that display error message if the username requirement is not succeeding
     const displayErrorMessage = (() => {
         if (!errors.username) {
             return null
         }
 
         if (errors.username.type === 'required') {
-            return <span>You must enter your name to proceed</span>
+            return <span>Enter name please...</span>
         }
     })()
-        //Effect - function to navigate to Translation Page if username is output
-
-        
 
         return (
         <>
             <form onSubmit={ handleSubmit(onSubmit) }>
                 <label htmlFor='username'>⌨ | </label>
                 <input 
-                type = 'text' 
-                {...register('username', usernameRequirement)} 
-                placeholder = "What's your name?"/>
-
-                <button type = 'submit'>▶</button><br></br>
+                    type = 'text' 
+                    placeholder = "What's your name?"
+                    {...register('username', usernameRequirement)} 
+                />
+                <button type = 'submit' disabled={ loadingText }>▶</button><br></br>
                 { displayErrorMessage }
+                { loadingText && <p>To be continued...</p>}
+                { errorMessageApi && <p>{ errorMessageApi }</p>}
             </form>
         </>
         )
