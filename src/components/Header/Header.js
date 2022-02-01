d332205730190affb6223deae05594281f0acbe8
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-02-01 16:13:27 +0100

    feat: Link in Header to profile.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
 import './HeaderStyle.css'
 const Header = () => {
+  const [user, setUser] = useUser();
   return (
   <div>
       <header>
           <h1 className='header-text'>Lost In Translation</h1>
+          {user !== null &&
+            <Link to="/profile">{user.username}</Link>
+          }
       </header>
   </div>
   );
